6bcaa957ccb19d24fefe8fd547e351d6f4f93e25
Cristian Alexis Montoya Arguello <alexis.montoya97@hotmail.com>
2022-04-22 13:04:22 -0500

    agregando key para la api themoviedb

--- src/views/MovieDetailView.vue
+++ src/views/MovieDetailView.vue
@@ -62,7 +62,7 @@ export default {
     async getMovieDetail(idMovie) {
       try {
         const response = await fetch(
-          `https://api.themoviedb.org/3/movie/${idMovie}?api_key=${this.api_key}&language=en-ES`
+          `https://api.themoviedb.org/3/movie/${idMovie}?api_key=ecaa4965ffbe006e64de9e316960fd4b&language=en-ES`
         );
         const array = await response.json();
 
